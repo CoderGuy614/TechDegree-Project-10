52ab5e9b0a7092c9768222a300b0ba25327c99a9
CoderGuy614 <jlutz.110@gmail.com>
2020-04-03 20:38:51 +0800

    changed route names to match grading criteria

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
 //TreeHouse TechDegree Project 10 By Jonathan Lutz - March 2020
 //Import components to app
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import CourseDetail from "./components/CourseDetail"
-import Courses from "./components/Courses"
-import CreateCourse from "./components/CreateCourse"
-import UpdateCourse from "./components/UpdateCourse"
-import DeleteCourse from "./components/DeleteCourse"
-import UserSignUp from "./components/UserSignUp"
-import UserSignIn from "./components/UserSignIn"
-import UserSignOut from "./components/UserSignOut"
-import withContext from "./Context"
-import UnhandledError from "./components/UnhandledError"
-import NotFound from "./components/NotFound"
-import PrivateRoute from './PrivateRoute';
-import Forbidden from "./components/Forbidden"
+import CourseDetail from "./components/CourseDetail";
+import Courses from "./components/Courses";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from "./components/UpdateCourse";
+import DeleteCourse from "./components/DeleteCourse";
+import UserSignUp from "./components/UserSignUp";
+import UserSignIn from "./components/UserSignIn";
+import UserSignOut from "./components/UserSignOut";
+import withContext from "./Context";
+import UnhandledError from "./components/UnhandledError";
+import NotFound from "./components/NotFound";
+import PrivateRoute from "./PrivateRoute";
+import Forbidden from "./components/Forbidden";
 // import css
-import './App.css';
+import "./App.css";
 // Add context to components that require it
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
-const CreateCourseWithContext = withContext(CreateCourse)
-const CourseDetailWithContext = withContext(CourseDetail)
-const DeleteCourseWithContext = withContext(DeleteCourse)
-const ForbiddenWithContext = withContext(Forbidden)
-const CoursesWithContext = withContext(Courses)
-const UpdateCourseWithContext = withContext(UpdateCourse)
+const CreateCourseWithContext = withContext(CreateCourse);
+const CourseDetailWithContext = withContext(CourseDetail);
+const DeleteCourseWithContext = withContext(DeleteCourse);
+const ForbiddenWithContext = withContext(Forbidden);
+const CoursesWithContext = withContext(Courses);
+const UpdateCourseWithContext = withContext(UpdateCourse);
 
 // Routing for all components
 function App() {
@@ -34,18 +34,22 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-        <Route path="/signin" component={UserSignInWithContext} />
-        <Route path="/signup" component={UserSignUpWithContext} />
-        <Route path="/signout" component={UserSignOutWithContext} />
-        <Route path="/error" component={UnhandledError} />
-        <Route path="/notfound" component={NotFound} />
-        <Route path="/forbidden" component={ForbiddenWithContext} />
-        <Route path="/delete-course/:id" component={DeleteCourseWithContext} exact />
-        <PrivateRoute path="/courses/update-course/:id" component={UpdateCourseWithContext} exact />
-        <PrivateRoute path="/create-course/" component={CreateCourseWithContext} />
-        <Route path="/courses/:id" component={CourseDetailWithContext} />
-        <Route path="/" component={CoursesWithContext} exact/>
-        <Route component={NotFound} />
+          <Route path="/signin" component={UserSignInWithContext} />
+          <Route path="/signup" component={UserSignUpWithContext} />
+          <Route path="/signout" component={UserSignOutWithContext} />
+          <Route path="/error" component={UnhandledError} />
+          <Route path="/notfound" component={NotFound} />
+          <Route path="/forbidden" component={ForbiddenWithContext} />
+          <Route path="/delete/:id" component={DeleteCourseWithContext} exact />
+          <PrivateRoute
+            path="/courses/update/:id"
+            component={UpdateCourseWithContext}
+            exact
+          />
+          <PrivateRoute path="/create/" component={CreateCourseWithContext} />
+          <Route path="/courses/:id" component={CourseDetailWithContext} />
+          <Route path="/" component={CoursesWithContext} exact />
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     </div>
--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,100 +1,119 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 import { Link, Redirect } from "react-router-dom";
-import withContext from "../Context"
-import Header from "../components/Header"
-
+import withContext from "../Context";
+import Header from "../components/Header";
 
 import "../styles/global.css";
 const HeaderWithContext = withContext(Header);
 
 // Utilize React MarkDown to handle markdown input in the materialsNeeded field
-const ReactMarkdown = require('react-markdown')
+const ReactMarkdown = require("react-markdown");
 // Defining and exporting the class component
 export default class CourseDetail extends Component {
-    state = {
-        redirect: false, 
-        course: {
-          user:{}
-        }
-        
-    };
-// When the component loads, get the course that matches the id in the url params and set it's data into the state
-    componentDidMount() {
-        axios.get(`http://localhost:5000/api/courses/${this.props.match.params.id}`).then(course => {
-          if(course.data){
-            this.setState({course: course.data})
-          } else {
-            this.setState({redirect:true})
-          }
-            })      
+  state = {
+    redirect: false,
+    course: {
+      user: {}
     }
-//If a user tries to navigate to a non-existent courser route, redirect them to the 404 Not Found Page.
-    render() {
-      if(this.state.redirect){
-        return <Redirect to ="/notfound" />
-      } else {
+  };
+  // When the component loads, get the course that matches the id in the url params and set it's data into the state
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/courses/${this.props.match.params.id}`)
+      .then(course => {
+        if (course.data) {
+          this.setState({ course: course.data });
+        } else {
+          this.setState({ redirect: true });
+        }
+      });
+  }
+  //If a user tries to navigate to a non-existent courser route, redirect them to the 404 Not Found Page.
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/notfound" />;
+    } else {
       const { context } = this.props;
       const authUser = context.authenticatedUser;
-      const courseOwnerId=this.state.course.user.id
-        return (
-            <div id="root">
-      <div>
-        <HeaderWithContext />
-      <div>
-        <div className="actions--bar">
-          <div className="bounds">
-            <div className="grid-100">
-            {/* Conditionally render the update and delete buttons depending on the current user and course owner. */}
-            {authUser && authUser.id === courseOwnerId ? 
-            <React.Fragment>
-              <span> 
-                <Link className="button" to={`update-course/${this.state.course.id}`}>Update Course</Link>
-                <Link className="button" to={`/delete-course/${this.state.course.id}`}>Delete Course</Link>
-              </span>
-              <Link className="button button-secondary" to="/">Return to List</Link>
-            </React.Fragment> 
-            :
-            <React.Fragment>
-              <Link className="button button-secondary" to="/">Return to List</Link>
-            </React.Fragment> 
-            }
+      const courseOwnerId = this.state.course.user.id;
+      return (
+        <div id="root">
+          <div>
+            <HeaderWithContext />
+            <div>
+              <div className="actions--bar">
+                <div className="bounds">
+                  <div className="grid-100">
+                    {/* Conditionally render the update and delete buttons depending on the current user and course owner. */}
+                    {authUser && authUser.id === courseOwnerId ? (
+                      <React.Fragment>
+                        <span>
+                          <Link
+                            className="button"
+                            to={`update/${this.state.course.id}`}
+                          >
+                            Update Course
+                          </Link>
+                          <Link
+                            className="button"
+                            to={`/delete/${this.state.course.id}`}
+                          >
+                            Delete Course
+                          </Link>
+                        </span>
+                        <Link className="button button-secondary" to="/">
+                          Return to List
+                        </Link>
+                      </React.Fragment>
+                    ) : (
+                      <React.Fragment>
+                        <Link className="button button-secondary" to="/">
+                          Return to List
+                        </Link>
+                      </React.Fragment>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="bounds course--detail">
+                <div className="grid-66">
+                  <div className="course--header">
+                    <h4 className="course--label">Course</h4>
+                    <h3 className="course--title">{this.state.course.title}</h3>
+                    <p>
+                      By {this.state.course.user.firstName}{" "}
+                      {this.state.course.user.lastName}{" "}
+                    </p>
+                  </div>
+                  <div className="course--description">
+                    <p>{this.state.course.description}</p>
+                  </div>
+                </div>
+                <div className="grid-25 grid-right">
+                  <div className="course--stats">
+                    <ul className="course--stats--list">
+                      <li className="course--stats--list--item">
+                        <h4>Estimated Time</h4>
+                        <h3>{this.state.course.estimatedTime}</h3>
+                      </li>
+                      <li className="course--stats--list--item">
+                        <h4>Materials Needed</h4>
+                        <ul>
+                          {/* Support for markdown input in this field */}
+                          <ReactMarkdown
+                            source={this.state.course.materialsNeeded}
+                          />
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="bounds course--detail">
-          <div className="grid-66">
-            <div className="course--header">
-              <h4 className="course--label">Course</h4>
-              <h3 className="course--title">{this.state.course.title}</h3>
-              <p>By {this.state.course.user.firstName} {this.state.course.user.lastName} </p>
-            </div>
-            <div className="course--description">
-              <p>{this.state.course.description}</p>
-            </div>
-          </div>
-          <div className="grid-25 grid-right">
-            <div className="course--stats">
-              <ul className="course--stats--list">
-                <li className="course--stats--list--item">
-                  <h4>Estimated Time</h4>
-                  <h3>{this.state.course.estimatedTime}</h3>
-                </li>
-                <li className="course--stats--list--item">
-                  <h4>Materials Needed</h4>
-                  <ul>
-                  {/* Support for markdown input in this field */}
-                    <ReactMarkdown source={this.state.course.materialsNeeded} />
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-        )
+      );
     }
   }
 }
